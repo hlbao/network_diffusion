f8bcd4ae493a4e7218150604a5329f26f5b13330
Honglin Bao <baohongl@msu.edu>
2021-01-06 15:49:00 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -148,3 +148,128 @@ class Decision:
 
         return agents
 
+class Epidemics:
+    """Functions related to SIR dynamics"""
+
+    def __init__(self, effectiveness):
+        self.beta = 2                          # Infection probability
+        self.mu = 0.33                         # Recovery probability
+        self.num_initI = 5                     # Number of initially infected people
+        self.effectiveness = effectiveness     # Effectiveness of Vaccination
+        self.total_transition_probability = 0  # Sum of the transition probability over all agents
+        self.accum_days = 0                    # Accumulative day calculated by Gillespie algorithm(count_day method)
+
+    def reset_day(self):
+        """Initialize accum_day at the beginning of each season"""
+
+        self.accum_days = 0
+
+    def init_state(self, agents):
+        """Set initial state for all agents & call self.transition_probability"""
+
+        NV_id = [agent.id for agent in agents if agent.strategy == NV]
+        init_infectors_id = rnd.sample(NV_id, k = self.num_initI)  # Initial infected people should be selected from non-vaccinators
+
+        # Set initial state
+        for focal in agents:
+            if focal.id in init_infectors_id:
+                focal.state = I
+            else:
+                focal.state = S
+
+        # Count the number of neighboring I agents
+        for focal in agents:
+            focal.num_I = len([neighbor for neighbor in focal.neighbors if neighbor.state == I])
+
+        # Set transition probability for all agents
+        agents = self.transition_probability(agents)
+
+        return agents
+
+    def transition_probability(self, agents):
+        """Set transition probabilities for all agents and take the sum of them"""
+
+        self.total_transition_probability = 0
+
+        for focal in agents:
+            if focal.state == S:
+                focal.transition_probability = self.beta*focal.num_I
+
+            if focal.state == I:
+                focal.transition_probability = self.mu
+
+            if focal.state == R:
+                focal.transition_probability = 0
+
+            self.total_transition_probability += focal.transition_probability
+
+        return agents
+
+    def state_change(self, agents):
+        """Update SIR state based on Gillespie Algorithm"""
+
+        rand_num = rnd.random()
+        probability = 0
+
+        for focal in [agent for agent in agents if agent.state != R and agent.transition_probability != 0]:
+            probability += focal.transition_probability/self.total_transition_probability
+
+            if rand_num <= probability:  # This random number is fixed for all agents
+                previous_transition_prob = focal.transition_probability
+
+                # For Failed SV agent
+                if focal.state == S and focal.strategy == V:
+                    if rnd.random() > self.effectiveness:
+                        focal.next_state = I
+                        focal.transition_probability = self.mu
+
+                        for neighbor in focal.neighbors:
+                            neighbor.num_I += 1
+                            self.update_total_transition_probability(neighbor)
+
+                    else:
+                        # Not necessary but for safety
+                        focal.next_state = S
+                        focal.transition_probability = self.beta*focal.num_I
+
+                # For NV agent
+                if focal.state == S and focal.strategy == NV:
+                    focal.next_state = I
+                    focal.transition_probability = self.mu
+
+                    for neighbor in focal.neighbors:
+                        neighbor.num_I += 1
+                        self.update_total_transition_probability(neighbor)
+
+                # For I agent
+                if focal.state == I:
+                    focal.next_state = R
+                    focal.transition_probability = 0
+
+                    for neighbor in focal.neighbors:
+                        neighbor.num_I -= 1
+                        self.update_total_transition_probability(neighbor)
+
+                focal.state = focal.next_state
+                self.total_transition_probability -= previous_transition_prob
+                self.total_transition_probability += focal.transition_probability
+
+                break  # only one agent should be picked up for one call of this method
+
+        return agents
+
+    def update_total_transition_probability(self, neighbor):
+        """Update total transition probability if the given neighbor is S state connected with newly generated I agent"""
+
+        if neighbor.state == S:
+            self.total_transition_probability -= neighbor.transition_probability
+            neighbor.transition_probability = self.beta*neighbor.num_I
+            self.total_transition_probability += neighbor.transition_probability
+
+    def count_day(self):
+        """Calculate accumulative days"""
+
+        delta_day = np.log(1/rnd.random())/self.total_transition_probability
+        self.accum_days += delta_day
+
+        return self.accum_days
